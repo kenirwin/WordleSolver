8baef1786b6fe9864558392aa1b3e878dcf7b8ae
Ken Irwin <ken@kenirwin.net>
2022-01-23 16:14:33 -0500

    reduce-wordlists can handle dos or linux files

--- reduce-wordlists.sh
+++ reduce-wordlists.sh
@@ -4,4 +4,4 @@ infile=$1
 outfile="${infile%.*}-reduced.txt"
 
 # compare infile and outfile and print out the intersection
-comm -12 <(sort wordlists/scrabble.txt) <(sort $infile) > $outfile
+comm -12 <(sort wordlists/scrabble.txt)  <( tr -d '\r' < $infile | sort ) > $outfile
